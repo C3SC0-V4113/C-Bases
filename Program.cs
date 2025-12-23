b39685db51d09173750f422566fb48751f609fb5
Francisco José Valle Cornejo <cescovalledev@gmail.com>
2025-12-23 15:26:19 -0500

    cleaning solution

--- Program.cs
+++ Program.cs
@@ -5,17 +5,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Explicit declaration of variables
-        int quantity = 5;
-        string message = "Hello, World!";
-        decimal prices = 19.99m;
-        WriteLine($"Message: {message}, Quantity: {quantity}, Prices: {prices:C}");
-
-        // Implicit declaration of variables
-        var greeting = "Welcome to C# programming!";
-        var percentage = 20.00m;
-        WriteLine($"Greeting: {greeting}, Percentage: {percentage}%");
-
         var laptop = new Product("Laptop", 1200);
         WriteLine(laptop.GetDescription());
         var soporte = new ServiceProduct("Soporte Técnico", 300, 30);
